10221f5dc1894262c7afea5dd6a9a34a4e6c999a
Gleb Poljakov <gleb.poljakov@gmail.com>
2014-02-01 02:38:02 +0600

    init commint

---
+++ controllers/Addresses.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ *	phpIPAM API class to work with addressess
+ *
+ * @author: gleb.poljakov
+ *
+ */
+
+class Addresses
+{
+	/* variables */
+	private $_params;
+	
+	/* set parameters, provided via post */
+	public function __construct($params)
+	{
+		$this->_params = $params;
+		
+		//ip address format, can be decimal or ip
+		if(!$this->_params['format'])								{ $this->_params['format'] = "decimal"; }
+		//verify IP address format
+		if(!($this->_params['format']=="decimal" || $this->_params['format']== "ip"))		{ throw new Exception('Invalid format'); }
+	}
+
+	/** 
+	* create new address
+	*/
+	public function createAddresses($_params)
+	{
+		/* not yet implemented */
+		throw new Exception('Action not yet implemented');
+	}
+
+
+	/** 
+	* read addresses
+	*/
+	public function readAddresses()
+	{
+		//init address class
+		$address = new Address();
+		
+		//set IP address format
+		$address->format = $this->_params['format'];
+		//set ip-address
+		$address->ip = $this->_params['ip'];
+
+		//fetch results
+		$res = $address->getAddress(); 
+		
+		//return address
+		return $res;
+	}
+
+
+	/** 
+	* update existing subnet 
+	*/
+	public function updateAddresses()
+	{
+		/* not yet implementes */
+		throw new Exception('Action not yet implemented');
+	}	
+	
+	
+	/** 
+	* delete subnet 
+	*/
+	public function deleteAddresses()
+	{
+		/* not yet implementes */
+		throw new Exception('Action not yet implemented');
+	}
+}
+
+/* debug
+
+include_once '../../functions/functions.php';
+include_once '../models/address.php';
+
+$params['format'] = 'ip';
+$params['ip'] = "192.168.1.1";
+
+$controller = new Addresses ($params);
+$action = 'readAddresses';
+print "AAA";
+if (method_exists($controller,$action)) {
+	print "true";
+
+	print_r($controller->$action());
+
+} else { print "False";};
+*/
+
+?>
---
+++ models/address.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ *	phpIPAM Address class
+ */
+
+class Address
+{	
+	// {{{ properties:
+	var $format = 'decimal';	//decimal=long or ip=dotted
+	var $ip = '';			//ip address in retrospective to $this->format
+	// }}}
+
+	// {{{ Get address details
+	public function getAddress() {
+	
+		// {{{ Check ip address validity
+		if ($this->format=='decimal') {
+			//Check ip validity for format
+			if (!is_numeric($this->ip)) 					{ throw new Exception('Invalid ip address - '.$this->ip.' in format '.$this->format); }
+		}
+		elseif($this->format=='ip') {
+			if (verifyCidr($this->ip.'/32', false))				{ throw new Exception('Invalid ip address - '.$this->ip.' in format '.$this->format); }
+
+			//Convert ip to long
+			$this->ip = Transform2decimal($this->ip);
+		}
+		// }}}
+
+		// {{ get addr details by ip
+		$res = getIpAddrDetailsByIp ($this->ip);
+		//throw new exception if not existing
+		if(sizeof($res)==0) 							{ throw new Exception('Address not existed'); }
+
+		//create object from results
+		foreach($res as $key=>$line) {
+			$this->$key = $line;
+		}
+
+		// {{{ If format==ip, then convert all Ids to names and etc.
+		if ($this->format=='ip'){
+			$this->state = reformatIPStateText($this->state);
+
+			//Get Switch and its params
+			$switchDetails = getSwitchById($this->switch);
+			$this->switch = $switchDetails['hostname'].' ['.$switchDetails['ip_addr'].']';
+
+			// {{{ Get Subnet and its params
+				$this->subnet = getSubnetDetailsById($this->subnetId);
+				$this->subnet['subnet'] = Transform2long($this->subnet['subnet']);
+
+				//Get Master Subnet Details
+				$masterSubnet = getSubnetDetailsById($this->subnet['masterSubnetId']);
+				if (!($masterSubnet['isFolder'])) $this->subnet['masterSubnet'] = Transform2long($masterSubnet['subnet']).'/'.$masterSubnet['mask'];
+
+				//Get VRF details
+				if ($this->subnet['vrfId']) {
+					$vrfDetails = getVRFDetailsById($this->subnet['vrfId']);
+					$this->subnet['vrfId'] = '['.$vrfDetails['rd'].'] '.$vrfDetails['name'].' ('.$vrfDetails['description'].')';
+				}
+
+				//Get Section name
+				$this->subnet['sectionId'] = getSectionDetailsById($this->subnet['sectionId'])['name'];
+
+				//Get VLAN Details
+				if ($this->subnet['vlanId']) {
+					$vlanDetails = subnetGetVlanDetailsById($this->subnet['vlanId']);
+					$this->subnet['vlanId'] = '['.$vlanDetails['number'].'] '.$vlanDetails['name'].' ('.$vlanDetails['description'].')';
+				}
+			// }}}
+
+
+		}
+		// }}}
+
+		// {{{ remove input parameters from output
+		unset($this->format);
+		unset($this->ip);
+		if ($this->format=='ip'){
+			unset($this->id);
+			unset($this->subnetId);
+		}
+		//}}}
+
+		//convert object to array
+		$result = $this->toArray($this, $format);
+
+		//return result
+		return $result;
+	}
+	// }}}
+
+	/**
+	* function to return multidimensional array
+	*/
+	public function toArray($obj, $format)
+	{
+		//if object create array
+		if(is_object($obj)) $obj = (array) $obj;
+		if(is_array($obj)) {
+			$arr = array();
+			foreach($obj as $key => $val) {
+				// output format
+				$arr[$key] = $this->toArray($val, $format);
+			}
+		}
+		else { 
+			$arr = $obj;
+		}
+		//return an array of items
+		return $arr;
+	}
+}
